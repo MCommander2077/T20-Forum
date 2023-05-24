af95aec69d853d2d6d2acfd3c193059290450274
MCommander2077 <2679935111@qq.com>
2023-05-24 22:40:24 +0800

    2023.5.24 20:09

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import os
 import platform
 
 from flask import (Flask, Response, make_response, redirect, render_template,
-                    request, url_for)
+                   request, url_for)
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
+
 password = 'T20-Forum_P^S5VV0rD'
 
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__, static_folder='static')
 app.secret_key = os.getenv('SECRET_KEY', 'secret_key')
+
 
 class MyForm(FlaskForm):
     name = StringField('Name:')
@@ -18,11 +21,20 @@ class MyForm(FlaskForm):
     downloadURL = StringField('downloadURL:')
     submit = SubmitField('Submit')
 
-
+'''
+# 装饰器在 HTTP 响应之前添加顶部导航栏
+@app.after_request
+def add_topnav(response):
+    Response.direct_passthrough = False
+    # 在 HTML 响应内容的前面添加 topnav.html 的内容
+    response.data = render_template('topnav.html') + response.data.decode('utf-8')
+    return response
+'''
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', topnav=render_template('topnav.html'))
+
 
 @app.route('/levels')
 def levels():
@@ -44,36 +56,38 @@ def levels():
     with open('data.txt', 'r') as f:
         for line in f:
             data.append(line.strip().split('|*|'))
-    return render_template('list.html', data=data[((page-1)*10):(page*10)] ,pagelist=pagelist)
+    return render_template('list.html', data=data[((page - 1) * 10):(page * 10)], pagelist=pagelist,
+                           topnav=render_template('topnav.html'))
+
 
 @app.route('/admin-login')
 def login_static():
     return render_template('login.html')
 
+
 @app.route('/admin-login', methods=['POST'])
 def login_post():
-    global password          # 声明全局变量'
+    global password  # 声明全局变量'
     form = MyForm()
-    pwd = request.form['password']    # 获取用户提交的密码
+    pwd = request.form['password']  # 获取用户提交的密码
 
-    if pwd == password:       # 判断密码是否正确
+    if pwd == password:  # 判断密码是否正确
         resp = make_response('<meta http-equiv="Refresh" content="0;url=../admin" />')  # 使用表单渲染模板
-        resp.set_cookie('password', pwd)     # 将密码存储在cookie中
+        resp.set_cookie('password', pwd)  # 将密码存储在cookie中
         return resp
     else:
-        return render_template('login.html', error='密码错误')
-
+        return render_template('login.html', error='密码错误', topnav=render_template('topnav.html'))
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    pwd_cookie = request.cookies.get('password')    # 获取密码cookie
+    pwd_cookie = request.cookies.get('password')  # 获取密码cookie
     print(pwd_cookie)
-    pwd = pwd_cookie if pwd_cookie else ''     # 如果cookie不存在，初始化密码为空
+    pwd = pwd_cookie if pwd_cookie else ''  # 如果cookie不存在，初始化密码为空
 
     form = MyForm()
 
-    if pwd == password:    # 如果密码正确，则返回内容
+    if pwd == password:  # 如果密码正确，则返回内容
         pass
     elif pwd == '':
         return '''
@@ -83,7 +97,7 @@ function myFunction()
 myFunction()
 </script>
 <meta http-equiv="Refresh" content="0;url=../admin-login" />
-'''    # 否则重定向至登录页面
+'''  # 否则重定向至登录页面
     else:
         return '''
 <script>
@@ -92,8 +106,7 @@ function myFunction()
 myFunction()
 </script>
 <meta http-equiv="Refresh" content="0;url=../admin-login" />
-'''    # 否则重定向至登录页面
-
+'''  # 否则重定向至登录页面
 
     if form.validate_on_submit():
         if not form.name.data == '' and not form.difficult.data == '' and not form.video.data == '' and not form.downloadURL.data == '' and not form.downloadURL.data == '':
@@ -109,12 +122,14 @@ myFunction()
 </script>
 <meta http-equiv="Refresh" content="0" />
 '''
-    return render_template('admin.html', form=form)
+    return render_template('admin.html', form=form, topnav=render_template('topnav.html'))
+
 
 @app.errorhandler(404)  # 传入要处理的错误代码
 def page_not_found(e):  # 接受异常对象作为参数
-    return render_template('404.html',error=e), 404  # 返回模板和状态码
+    return render_template('404.html', error=e, topnav=render_template('topnav.html')), 404  # 返回模板和状态码
+
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run()
+     app.run(debug=True,host='127.0.0.1', port=9809)
+    # app.run(host='127.0.0.1', port=9809)
