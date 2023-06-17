f9f11cc472f90daa3181f3c3eac9698d8dd66574
MCommander2077 <111177215+MCommander2077@users.noreply.github.com>
2023-06-17 20:07:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SECRET_KEY = "T20-Forum_P^S5VV0rD"
+SECRET_KEY = ""
 # DATABASE_CONFIG = 'mysql://T20-F:NFM2XffD5BG25BKj@127.0.0.1/T20-F' #mysql://用户名:密码@数据库地址/要连接的数据库
 userpass = 'mysql+pymysql://root:@'
 basedir = '127.0.0.1'
